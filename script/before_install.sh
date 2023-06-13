7eae932d1e02f81e465569e474443111c2b15d28
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-13 15:29:15 +0530

    removed sudo for npm run

--- script/before_install.sh
+++ script/before_install.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/nodejs_ec2_pipeline
 echo "pull the latest version of repository from the Github"
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
-sudo npm install
+npm install
