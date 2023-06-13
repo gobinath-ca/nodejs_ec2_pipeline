a8dffad52194a41d280b83dfa641ed07d6857fb8
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-13 14:51:14 +0530

    changes on scripts

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+cd /home/ubuntu/nodejs_ec2_pipeline
 echo "pull the latest version of repository from the Github"
-git init
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
-cd /home/ubuntu/nodejs_ec2_pipeline
 sudo npm install
