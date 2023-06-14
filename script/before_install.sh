4fb0a5a20a3ce02732564db801f697e8bce5805c
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 12:49:13 +0530

    full path changed

--- script/before_install.sh
+++ script/before_install.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/nodejs_ec2_pipeline
 echo "pull the latest version of repository from the Github"
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
-npm install
+/root/.nvm/versions/node/v20.3.0/bin/npm install
