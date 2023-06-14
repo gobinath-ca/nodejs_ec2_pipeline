48d08df7f8ab9873ffddf3f24b60fb1ff3574497
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 15:01:30 +0530

    permission change

--- script/application_stop.sh
+++ script/application_stop.sh
@@ -3,4 +3,4 @@ source /root/.bashrc
 echo "list of Node process running"
 sudo ps -aux | grep node | grep -v grep | wc -l
 echo "stopping the Node process that is running"
-pkill node
+sudo pkill node
--- script/before_install.sh
+++ script/before_install.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/nodejs_ec2_pipeline
 echo "list directory files"
 ls -ltr
 echo "pull the latest version of repository from the Github"
-sudo git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
+git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
 ls -ltr
 /root/.nvm/versions/node/v20.3.0/bin/npm install
