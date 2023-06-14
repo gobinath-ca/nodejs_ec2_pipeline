a1a973d71945d4ecb3a30acfa068dca69f269ec3
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 00:57:43 +0530

    sudo permission

--- script/before_container_start.sh
+++ script/before_container_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Pull image from ECR"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 176603075527.dkr.ecr.us-east-1.amazonaws.com
-docker pull 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
+sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 176603075527.dkr.ecr.us-east-1.amazonaws.com
+sudo docker pull 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
