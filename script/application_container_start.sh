d07416a12ca0abcb8a62194e8ac9facd5e189c8a
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 01:08:12 +0530

    sudo permissions for other scritp

--- script/application_container_start.sh
+++ script/application_container_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Build application container"
-docker run --restart=always -p 8081:8081 --name nodejs-demo-container-01 -d 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo
+sudo docker run --restart=always -p 8081:8081 --name nodejs-demo-container-01 -d 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo
--- script/application_stop.sh
+++ script/application_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "docker stop and remove"
-docker stop ndoejs-container-03
-docker rm ndoejs-container-03 --force
+sudo docker stop ndoejs-container-03
+sudo docker rm ndoejs-container-03 --force
