c12049deafd2d19d6e2f2d20072a506dd5ba2008
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 15:10:40 +0530

    permission change

--- script/before_install.sh
+++ script/before_install.sh
@@ -4,8 +4,8 @@ cd /home/ubuntu/nodejs_ec2_pipeline
 echo "list directory files"
 ls -ltr
 echo "Setting up the GitHub repo connection"
-sudo git config --global user.email "gobinath.gopalakrishnan@cloudarmee.com"
-sudo git config --global user.name "gobinath-ca"
+git config --global user.email "gobinath.gopalakrishnan@cloudarmee.com"
+git config --global user.name "gobinath-ca"
 echo "pull the latest version of repository from the Github"
 sudo git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
