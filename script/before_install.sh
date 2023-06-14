4adf605345dfb111d4154e7c5fc8f7233a553244
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 13:16:45 +0530

    list directory command add

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source /root/.bashrc
 cd /home/ubuntu/nodejs_ec2_pipeline
+echo "list directory files"
+ls -ltr
 echo "pull the latest version of repository from the Github"
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
 echo "install Node Dependencies"
