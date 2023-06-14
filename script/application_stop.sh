8aa9766c04270916351bbea494b690bed5193309
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 12:44:01 +0530

    source added

--- script/application_stop.sh
+++ script/application_stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /root/.bashrc
 echo "list of Node process running"
 sudo ps -aux | grep node | grep -v grep | wc -l
 echo "stopping the Node process that is running"
--- script/before_install.sh
+++ script/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /root/.bash_profile
+source /root/.bashrc
 cd /home/ubuntu/nodejs_ec2_pipeline
 echo "pull the latest version of repository from the Github"
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
