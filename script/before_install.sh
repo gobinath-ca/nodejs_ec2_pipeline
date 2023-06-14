c3e130bfdb7953eff6f9a4c18ae0f3c6e9943210
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-14 12:36:50 +0530

    changes for deploy error

--- script/before_install.sh
+++ script/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /root/.bash_profile
 cd /home/ubuntu/nodejs_ec2_pipeline
 echo "pull the latest version of repository from the Github"
 git pull https://github.com/gobinath-ca/nodejs_ec2_pipeline.git
