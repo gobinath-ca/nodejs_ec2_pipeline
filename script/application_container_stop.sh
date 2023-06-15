e480a4b31055d8e365217b8df177a11d8270a64d
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 11:45:04 +0530

    script changes

--- script/application_container_stop.sh
+++ script/application_container_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "docker stop and remove"
-sudo docker stop ndoejs-container-03
-sudo docker rm ndoejs-container-03 --force
+sudo docker stop demo001
+sudo docker rm demo001 --force
