9ddb008924c2fc20dd02c0df7ab51878b4549ee9
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 12:00:33 +0530

    index.js port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3100;
+const port = 8081;
 
 app.get('/', (req, res) => res.send('Welcome to NodeJs EC2 Deployment Session!'));
 
