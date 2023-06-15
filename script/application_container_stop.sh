0237826dca5416f99b624ca3260bccb74b5cc65b
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 12:10:16 +0530

    sudo permission revoke

--- script/application_container_stop.sh
+++ script/application_container_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "docker stop and remove"
-sudo docker stop nodejs-demo-container-01
-sudo docker rm nodejs-demo-container-01 --force
+docker stop nodejs-demo-container-01
+docker rm nodejs-demo-container-01 --force
