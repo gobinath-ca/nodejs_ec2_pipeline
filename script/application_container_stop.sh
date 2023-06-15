be2bad6329ca443df623317ad5f781771bf143d9
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-15 12:02:22 +0530

    script modification

--- script/application_container_stop.sh
+++ script/application_container_stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "docker stop and remove"
-sudo docker stop demo001
-sudo docker rm demo001 --force
+sudo docker stop nodejs-demo-container-01
+sudo docker rm nodejs-demo-container-01 --force
