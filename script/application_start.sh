253fbf6c2150eefaf4b722cdeff22b6142106ae9
Gobinath Gopalakrishnan <gobinath.gopalakrishnan@cloudarmee.com>
2023-06-16 10:45:51 +0530

    Code Modification

--- script/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Start Node.js Process"
-cd /home/ubuntu/nodejs_ec2_pipeline
-nohup node index.js &
-echo "Number of Node process running"
-sudo ps -aux | grep node | grep -v grep | wc -l
--- script/before_container_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Pull image from ECR"
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 176603075527.dkr.ecr.us-east-1.amazonaws.com
-sudo docker pull 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
