f4d0fbe19dc36e2b9f17f82ecf310b72f982f6e4
gobinath-ca <109211789+gobinath-ca@users.noreply.github.com>
2023-06-16 11:10:23 +0530

    Update application_container_start.sh

--- script/application_container_start.sh
+++ script/application_container_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Login to AWS ECR and Pull image from ECR"
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 176603075527.dkr.ecr.us-east-1.amazonaws.com
-sudo docker pull 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
+sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+sudo docker pull ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
 echo "Build application container"
-docker run --restart=always -p 8081:8081 --name nodejs-demo-container-01 -d 176603075527.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
+docker run --restart=always -p 8081:8081 --name nodejs-demo-container-01 -d ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/nodejs-demo:latest
